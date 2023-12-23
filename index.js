b2e819a9325f297a35f5f2ad899c20f09979a192
sabbir10s <sabbirahmed1023@gmail.com>
2023-12-23 22:36:48 +0600

    login api done

--- index.js
+++ index.js
@@ -22,42 +22,99 @@ async function run() {
         const productCollection = client.db("classicIt").collection("products");
         const userCollection = client.db("classicIt").collection("users");
 
-        // Get single product
-        app.get("/product", async (req, res) => {
+        // Get all product
+        app.get("/products", async (req, res) => {
             const products = await productCollection.find().toArray();
             res.send(products);
         })
 
+        // get single product
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        })
+
         // Create user 
         app.post("/register", async (req, res) => {
-            const { username, password } = req.body;
+            const { username, email, password } = req.body;
 
             try {
-                // Check if the username is already taken
-                const existingUser = await userCollection.findOne({ username });
+                const existingUser = await userCollection.findOne({ email });
                 if (existingUser) {
                     return res.status(400).json({ error: 'Username already exists' });
                 }
-
-                // Hash the password before saving to the database
                 const hashedPassword = await bcrypt.hash(password, 10);
-
-                // Create a new user document
                 const newUser = {
                     username,
+                    email,
                     password: hashedPassword,
+                    login: true
                 };
-
-                // Insert the new user into the users collection
                 const result = await userCollection.insertOne(newUser);
-
-                // Return a success message
                 res.status(200).json({ message: 'User registered successfully', insertedId: result.insertedId });
             } catch (error) {
                 console.error(error);
                 res.status(500).json({ error: 'Internal Server Error' });
             }
         });
+
+        // Login
+        app.post("/login", async (req, res) => {
+            const { email, password } = req.body;
+
+            try {
+                const user = await userCollection.findOne({ email });
+
+                if (!user) {
+                    return res.status(404).json({ error: 'User not found' });
+                }
+                const passwordMatch = await bcrypt.compare(password, user.password);
+                if (passwordMatch) {
+                    const currentTime = new Date();
+                    await userCollection.updateOne(
+                        { email },
+                        {
+                            $set: {
+                                login: true,
+                                lastLogin: currentTime,
+                            },
+                        }
+                    );
+
+                    // Send back additional information if needed
+                    res.status(200).json({ message: 'Login successful', loggedIn: true, lastLogin: currentTime });
+                } else {
+                    res.status(401).json({ error: 'Incorrect password' });
+                }
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
+
+
+        // Logout
+        app.post("/logout/:email", async (req, res) => {
+            const { email } = req.body;
+
+            try {
+                const user = await userCollection.findOne({ email });
+                if (!user) {
+                    return res.status(404).json({ error: 'User not found' });
+                }
+                await userCollection.updateOne({ email }, { $set: { login: false } });
+
+                res.status(200).json({ message: 'Logout successful', login: false });
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
+
+
 
 
     } finally {
