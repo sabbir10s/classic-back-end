4c8fe4f5d98a073a2718ef17ed4199376ec3fd5e
sabbir10s <sabbirahmed1023@gmail.com>
2023-12-23 18:56:05 +0600

    reg api added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 require('dotenv').config()
+const bcrypt = require('bcrypt');
 app.use(cors())
 app.use(express.json())
 
@@ -21,10 +22,43 @@ async function run() {
         const productCollection = client.db("classicIt").collection("products");
         const userCollection = client.db("classicIt").collection("users");
 
+        // Get single product
         app.get("/product", async (req, res) => {
             const products = await productCollection.find().toArray();
             res.send(products);
         })
+
+        // Create user 
+        app.post("/register", async (req, res) => {
+            const { username, password } = req.body;
+
+            try {
+                // Check if the username is already taken
+                const existingUser = await userCollection.findOne({ username });
+                if (existingUser) {
+                    return res.status(400).json({ error: 'Username already exists' });
+                }
+
+                // Hash the password before saving to the database
+                const hashedPassword = await bcrypt.hash(password, 10);
+
+                // Create a new user document
+                const newUser = {
+                    username,
+                    password: hashedPassword,
+                };
+
+                // Insert the new user into the users collection
+                const result = await userCollection.insertOne(newUser);
+
+                // Return a success message
+                res.status(200).json({ message: 'User registered successfully', insertedId: result.insertedId });
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
+
 
     } finally {
         //   await client.close();
