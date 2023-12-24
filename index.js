588316acc4577eb154ae871010258019f64e0f2b
sabbir10s <sabbirahmed1023@gmail.com>
2023-12-24 19:40:14 +0600

    token updated

--- index.js
+++ index.js
@@ -14,21 +14,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function authenticateToken(req, res, next) {
-    const token = req.headers.authorization;
-    if (!token) {
-        return res.status(401).json({ error: 'Unauthorized - No token provided' });
-    }
 
-    jwt.verify(token, 'your-secret-key', (err, user) => {
-        if (err) {
-            return res.status(403).json({ error: 'Forbidden - Invalid token' });
-        }
-        req.user = user;
-
-        next();
-    });
-}
 
 async function run() {
     try {
@@ -113,7 +99,7 @@ async function run() {
                 return res.status(401).json({ error: 'Unauthorized' });
             }
 
-            jwt.verify(token, 'secret-key', (err, user) => {
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
                 if (err) {
                     return res.status(403).json({ error: 'Forbidden' });
                 }
